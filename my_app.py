77f2d61b5d708148bf5a304773bfbe028cfc9c25
nebelgrau <nebelgrau@gmx.com>
2020-10-21 22:07:43 +0200

    added model

--- my_app.py
+++ my_app.py
@@ -2,14 +2,31 @@ import os
 
 from flask import Flask, render_template, request, redirect
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
 from make_bokeh_chart import simple_bokeh_chart
 
 # define paths to project and database
 project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = 'sqlite:///{}'.format(os.path.join(project_dir, 'products.db'))
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file # tell the app where the database is
 
 bootstrap = Bootstrap(app) # initialize Bootstrap
+
+db = SQLAlchemy(app) # initialize a connection to the database
+
+class Product(db.Model):
+	__tablename__ = products
+	index = db.Column(db.Integer(), unique=True, nullable=False, primary_key=True)
+	category = db.Column(db.String(40), unique = False, nullable=False) # e.g. TV, radio...
+	brand = db.Column(db.String(40), unique = False, nullable=False)
+	quality_score = db.Column(db.Integer, unique = False, nullable=True)
+	price_score = db.Column(db.Integer, unique = False, nullable=True)
+
+	def __repr__(self):
+		return "<Index: {:05d}, Category: {}, Brand: {}, Quality score: {}, Price score: {}".format(self.index, self.category, self.brand, self.quality_score,self.price_score)
+
 
 
 @app.route('/')
