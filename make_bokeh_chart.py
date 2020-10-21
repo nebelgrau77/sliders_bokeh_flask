db1684cf235b2ff429325cc332ffffdead9fb9dc
nebelgrau <nebelgrau@gmx.com>
2020-10-21 21:31:58 +0200

    added text with source

--- make_bokeh_chart.py
+++ make_bokeh_chart.py
@@ -15,14 +15,21 @@ def simple_bokeh_chart():
 
 	sample_text = ["text"]
 
-	source = ColumnDataSource(dict(text = sample_text))
-
 	fig = figure(title='Test', plot_width=500, plot_height=500,toolbar_location=None)	
+
+	xs = [1,1,3,3]
+	ys = [1,3,1,3]
+	labels = ['SW', 'NW', 'SE', 'NE']
+
+	source = ColumnDataSource(dict(x=xs, y=ys, label=labels))
+
 	
 	fig.rect(1,1, 2,2, fill_color = c_red, fill_alpha = 0.8, line_color = c_white)
 	fig.rect(1,3, 2,2, fill_color = c_yellow, fill_alpha = 0.8, line_color = c_white)
 	fig.rect(3,1, 2,2, fill_color = c_yellow, fill_alpha = 0.8, line_color = c_white)
 	fig.rect(3,3, 2,2, fill_color = c_green, fill_alpha = 0.8, line_color = c_white)
+	
+	fig.text(x='x', y='y', text='label', text_font_style="bold", text_align='left', text_baseline="middle", source = source)
 
 	fig.outline_line_color = None
 	fig.grid.grid_line_color = None
@@ -30,9 +37,6 @@ def simple_bokeh_chart():
 	fig.axis.major_tick_line_color = None
 	fig.axis.minor_tick_line_color = None
 	fig.axis.major_label_text_color = None
-
-
-	fig.text(x=1, y=1, text=sample_text, text_font_style="bold", text_align='left', text_baseline="middle")
 
 	#grab the static resources
 	js_resources = INLINE.render_js()
